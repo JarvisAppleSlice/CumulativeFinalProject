eb5ccd673730db5c38f705828bf32af988c974cc
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-04-14 15:14:09 -0600

    Start

---
+++ spec/sort.testimonials.spec.js
@@ -0,0 +1,18 @@
+describe("sortTestimonials", () => {
+	
+      const unsortedTestimonials =[
+        {feedback: "Incredible! Just amazing...", rating: 5}
+        {feedback: "At least it was cheap.", rating: 3}
+        {feedback: "That one guy was too funny for me to concentrate.", rating: 0}
+        ]
+    
+    it("sorts testimonials by feedback alphabetically", () => {
+      const expectedSorting =
+    });
+	it("sorts testimonials by decreasing rating", () => {
+
+    });
+	it("sorts testimonials by increasing rating", () => {
+
+    });
+});
